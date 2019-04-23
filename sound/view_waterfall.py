5cb75951e268b23ad0417ddc51c15fa2901e34ee
sourceperl <loic.celine@free.fr>
2019-04-23 18:10:00 +0200

    add waterfall viewer

---
+++ sound/view_waterfall.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+import os.path
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+
+
+# some function
+def is_valid_file(filename):
+    if not os.path.exists(filename):
+        raise argparse.ArgumentTypeError("file %s does not exist" % filename)
+    else:
+        return filename
+
+
+if __name__ == '__main__':
+    # parse arg
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="input wav file",
+                        type=lambda f: is_valid_file(f))
+    args = parser.parse_args()
+    # build wav spectrogram
+    sample_rate, samples = wavfile.read(args.filename)
+    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate, nperseg=4096)
+    # plot waterfall
+    plt.pcolormesh(times, frequencies, spectrogram)
+    plt.colorbar()
+    plt.ylabel("Frequency [Hz]")
+    plt.xlabel("Time [sec]")
+    plt.show()
