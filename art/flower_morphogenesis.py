853ac6a793fbc8e9a784cf6f9136ee67cabe3a5d
sourceperl <loic.celine@free.fr>
2020-04-29 16:38:48 +0200

    fix flower morphogenesis

--- art/flower_morphogenesis.py
+++ art/flower_morphogenesis.py
@@ -12,7 +12,7 @@ for p1_m, p2_m in [(3, 5), (5, 8), (8, 13), (13, 21), (21, 34)]:
     # plot setup
     plt.axes().set_aspect(1)
     plt.axis('off')
-    plt.title('flower for (%i,%i)' % (p1_m, p2_m))
+    plt.title('flower morphogenesis for (%i,%i)' % (p1_m, p2_m))
     # draw
     for x in np.linspace(0, 2 * np.pi, 610):
         p1 = Point(np.cos(p1_m * x), np.sin(p1_m * x))
