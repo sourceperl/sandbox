6afe5b2412317559506c5b253003c6d1c85fe05a
sourceperl <loic.celine@free.fr>
2021-06-30 15:37:22 +0200

    improve micropython grove LCD RGB class

--- micropython/rp2040/main.py
+++ micropython/rp2040/main.py
@@ -5,7 +5,7 @@
 from machine import I2C, Pin
 from utime import sleep
 import urandom
-from grove_lcd_i2c import Grove_LCD_I2C
+from grove_lcd_rgb_i2c import Grove_LCD_RGB_I2C
 
 # some const
 I2C_ID = 0
@@ -15,21 +15,16 @@ I2C_SCL = 1
 # init I2C bus
 i2c = I2C(I2C_ID, sda=Pin(I2C_SDA), scl=Pin(I2C_SCL))
 # init LCD display
-lcd = Grove_LCD_I2C(i2c)
+lcd = Grove_LCD_RGB_I2C(i2c)
 
 # debug: scan I2C bus
 #print('node(s) on I2C bus:', [hex(i) for i in i2c.scan()])
 
 # turn on RGB backlight
-lcd.setRGB(0x00, 0x60, 0x00)
-
-# startup message
-lcd.home()
-lcd.write('startup...')
-sleep(1.5)
+lcd.setRGB(0x80, 0x80, 0x20)
 
 # display the random number
-lcd.clear()
+lcd.home()
 lcd.write('Random number:')
 
 # main loop
