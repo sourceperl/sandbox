7451496d65f23b7ad47fa261b76c45df3a05ff01
l.lefebvre <loic.celine@free.fr>
2021-11-12 20:27:36 +0100

    update mbus_monitor.py

--- serial/mbus_monitor.py
+++ serial/mbus_monitor.py
@@ -44,11 +44,11 @@ if __name__ == '__main__':
 
     # some vars
     t_eof = 18 * 3.5 * 1/(BAUDRATE/10)
-    crc_ok_count = 0
-    crc_ko_count = 0
-    frame_nb = 0
     t_last_rx = None
     b_rx_buf = bytearray()
+    f_ok_count = 0
+    f_ko_count = 0
+    f_nb = 0
 
     # flush buffer
     s.read_all()
@@ -67,20 +67,22 @@ if __name__ == '__main__':
             # test if end of frame occur
             if (time.monotonic() - t_last_rx) > t_eof:
                 # skip first frame (probably partial)
-                if frame_nb > 0:
-                    # frame CRC check
-                    if check_crc_ok(b_rx_buf):
-                        crc_status = 'OK'
-                        crc_ok_count += 1
+                if f_nb > 0:
+                    # frame length and CRC check
+                    if len(b_rx_buf) > 4 and check_crc_ok(b_rx_buf):
+                        f_status = 'OK'
+                        f_ok_count += 1
                     else:
-                        crc_status = 'ERR'
-                        crc_ko_count += 1
+                        f_status = 'ERR'
+                        f_ko_count += 1
                     # dump frame
                     hex_frame = frame2hex(b_rx_buf)[:80]
                     date_str = datetime.now().isoformat(sep=' ', timespec='milliseconds')
-                    print(f'{date_str} [#{frame_nb:>6}] [{len(b_rx_buf):>3}] [{crc_status:<3}] [{crc_ok_count:>5}|{crc_ko_count:>5}] [eof {t_eof:0.4f}]: {hex_frame}')
+                    dump = f'{date_str} [#{f_nb:>6}] [{len(b_rx_buf):>3}] [{f_status:<3}] ' \
+                           f'[{f_ok_count:>6}|{f_ko_count:>6}] [eof {t_eof:0.4f}]: {hex_frame}'
+                    print(dump)
                 # frame index
-                frame_nb += 1
+                f_nb += 1
                 # clear the rx buffer
                 b_rx_buf.clear()
         # avoid high CPU consumption
