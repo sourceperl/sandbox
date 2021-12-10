31c829c21800943fd525af28462ad5ca42b5b919
sourceperl <loic.celine@free.fr>
2021-12-10 09:29:09 +0100

    minor fix

--- sqlite/basic.py
+++ sqlite/basic.py
@@ -8,7 +8,6 @@
 from random import randint
 import sqlite3
 
-
 # connect to DB store in example.db file
 db = sqlite3.connect('./example.db')
 
@@ -19,8 +18,8 @@ dbc = db.cursor()
 dbc.execute('CREATE TABLE IF NOT EXISTS `test` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `value` TEXT)')
 # populate it with random values
 for _ in range(1_000):
-   v = randint(0, 0xFFFF_FFFF_FFFF_FFFF)
-   dbc.execute(f"INSERT INTO `test` (`value`) VALUES ('{v:016x}')")
+    v = randint(0, 0xFFFF_FFFF_FFFF_FFFF)
+    dbc.execute(f"INSERT INTO `test` (`value`) VALUES ('{v:016x}')")
 
 # commit all changes on DB and close it
 db.commit()
