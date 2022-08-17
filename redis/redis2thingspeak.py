d1301584178f002ce4beb2813a4a15015be9c948
sourceperl <loic.celine@free.fr>
2022-08-17 14:33:55 +0200

    add redis/

---
+++ redis/redis2thingspeak.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""Push some data from redis keys to thingspeak server."""
+
+import time
+import logging
+import urllib.error
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from private_data import API_KEY
+# sudo apt install python3-redis
+import redis
+# sudo apt install python3-schedule
+import schedule
+
+
+# define schedule jobs
+def thingspeak_job():
+    """Periodic thingspeak update job."""
+    try:
+        # init thingspeak data dict
+        data_d = dict()
+        # populate it with valid redis values
+        try:
+            r_value = int(red.get('wobbe_ready_bool'))
+            if r_value not in [0, 1]:
+                raise ValueError
+            data_d['field1'] = r_value
+        except (TypeError, ValueError):
+            logging.warning(f'unable to process redis key "wobbe_ready_bool" value must be 0 or 1')
+        try:
+            data_d['field2'] = round(float(red.get('wobbe_value_float')))
+        except (TypeError, ValueError):
+            logging.warning(f'unable to process redis key "wobbe_value_float" value must be a valid float')
+        # add API key
+        data_d['api_key'] = API_KEY
+        # do thingspeak request
+        resp = urlopen(f'https://api.thingspeak.com/update?{urlencode(data_d)}', timeout=5.0)
+        # print request status
+        try:
+            # HTTP request return current entry ID or 0 on error
+            entry_id = int(resp.read())
+            if entry_id < 1:
+                raise ValueError
+            logging.info(f'successful data update to entry ID: {entry_id}')
+        except ValueError:
+            logging.warning(f'web server refused to update data')
+    except redis.RedisError as e:
+        logging.error(f'redis error occur: {e!r}')
+    except urllib.error.URLError as e:
+        logging.error(f'network error occur: {e!r}')
+
+
+if __name__ == '__main__':
+    # logging setup
+    logging.basicConfig(format='%(asctime)s - %(levelname)-8s - %(message)s', level=logging.INFO)
+    logging.getLogger('schedule').setLevel(logging.WARNING)
+    # log startup
+    logging.info('export-thingspeak-app started')
+
+    # init redis DB
+    red = redis.StrictRedis()
+
+    # schedule config
+    schedule.every(2).minutes.do(thingspeak_job)
+
+    # first call
+    thingspeak_job()
+
+    # main loop
+    while True:
+        schedule.run_pending()
+        time.sleep(1.0)
