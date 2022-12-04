dab3f2f0f5e83ba062c419fcabf0fbbb3cfd243d
sourceperl <3809271+sourceperl@users.noreply.github.com>
2022-12-04 18:43:15 +0100

    update show_cv.py

--- plot/cv_valve_check/show_cv.py
+++ plot/cv_valve_check/show_cv.py
@@ -1,43 +1,49 @@
 #!/usr/bin/env python3
 
+"""
+Some function to deal with Cv flow coefficient of gas valve.
+
+https://www.swagelok.com/downloads/webcatalogs/EN/MS-06-84.pdf
+"""
+
 import csv
-import matplotlib.pyplot as plt
 import math
+import matplotlib.pyplot as plt
 
 
 # some functions
-def celsius_to_kelvin(deg_c):
+def celsius_to_kelvin(deg_c: float) -> float:
+    """Conversion from degree Celsius to Kelvin"""
     return deg_c + 273.15
 
 
-def is_subsonic(p_up_bara, p_down_bara):
+def is_subsonic(p_up_bara: float, p_down_bara: float) -> bool:
+    """Obtain subsonic state for current pressures"""
     return p_down_bara > p_up_bara / 2
 
 
-def valve_flow(cv, p_up_bara, p_down_bara, position=100.0, t_deg_c=8.0, sg=0.64):
-    # compute flow in linear process valve
-    t_deg_k = celsius_to_kelvin(t_deg_c)
-    kv = 0.857 * cv
+def valve_flow(cv: float, p_up_bara: float, p_down_bara: float, t_deg_c: float = 6.0, sg: float = 0.554) -> float:
+    """Compute flow rate (nm3/h) in a valve from it's Cv"""
+    t_k = celsius_to_kelvin(t_deg_c)
+    dp_bar = p_up_bara - p_down_bara
     # circulation below or over critical point
     if is_subsonic(p_up_bara, p_down_bara):
-        delta_p_bar = p_up_bara - p_down_bara
-        q_nm3_h = 514 * kv * (position / 100.0) * math.sqrt((delta_p_bar * p_down_bara) / (sg * t_deg_k))
+        return 417 * cv * p_up_bara * (1 - ((2 * dp_bar) / (3 * p_up_bara))) * \
+               math.sqrt(dp_bar / (p_up_bara * sg * t_k))
     else:
-        q_nm3_h = 257 * kv * (position / 100.0) * p_up_bara * 1 / math.sqrt(sg * t_deg_k)
-    return q_nm3_h
+        return 0.471 * 417 * cv * p_up_bara * math.sqrt(1 / (sg * t_k))
 
 
-def valve_cv(q_nm3_h, p_up_bara, p_down_bara, position=100.0, t_deg_c=8.0, sg=0.64):
-    # compute cv for a linear process valve
-    t_deg_k = celsius_to_kelvin(t_deg_c)
-    delta_p_bar = p_up_bara - p_down_bara
+def valve_cv(q_nm3_h: float, p_up_bara: float, p_down_bara: float, t_deg_c: float = 6.0, sg: float = 0.554) -> float:
+    """Compute Cv of a valve from its flow rate (nm3/h)"""
+    t_k = celsius_to_kelvin(t_deg_c)
+    dp_bar = p_up_bara - p_down_bara
     # circulation below or over critical point
     if is_subsonic(p_up_bara, p_down_bara):
-        kv = q_nm3_h * (100.0 / (514 * position)) * math.sqrt((sg * t_deg_k) / (delta_p_bar * p_down_bara))
+        return q_nm3_h / (417 * p_up_bara * (1 - ((2 * dp_bar) / (3 * p_up_bara))) *
+                          math.sqrt(dp_bar / (p_up_bara * sg * t_k)))
     else:
-        kv = q_nm3_h * (100.0 / (257 * p_up_bara * position)) * math.sqrt(sg * t_deg_k)
-    cv = kv / 0.857
-    return cv
+        return q_nm3_h / (0.471 * 417 * p_up_bara * math.sqrt(1 / (sg * t_k)))
 
 
 if __name__ == '__main__':
@@ -52,12 +58,12 @@ if __name__ == '__main__':
             dp = (float(row['P amont']) / 10) - (float(row['P aval']) / 10)
             p_amt = float(row['P amont']) / 10
             p_avl = float(row['P aval']) / 10
-            qt = valve_flow(cv=48, p_up_bara=p_amt, p_down_bara=p_avl, position=pos)
+            qt = valve_flow(cv=48 * pos / 100, p_up_bara=p_amt, p_down_bara=p_avl)
             qt_l.append(qt)
 
     # plot it
-    plt.scatter(pos_l, qi_l, marker='o', label='Qi')
-    plt.scatter(pos_l, qt_l, marker='^', label='Qt')
+    plt.scatter(pos_l, qi_l, marker='o', label='Meter flow')
+    plt.scatter(pos_l, qt_l, marker='^', label='Calculated flow')
     plt.xlabel('Position (%)')
     plt.ylabel('Q (nm3/h)')
     plt.legend()
