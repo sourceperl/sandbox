60bc1aff06409185bb1178f1d4d417e745752807
sourceperl <3809271+sourceperl@users.noreply.github.com>
2022-12-06 11:32:39 +0100

    update flow_valve nano hmi

--- hmi/nano/flow_valve/app.py
+++ hmi/nano/flow_valve/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+from collections import deque
 import csv
 from datetime import datetime
 from os import chdir
 from os.path import abspath, dirname
 import time
 import threading
-from typing import Any
 from lib.modbus import FloatModbusClient
 from lib.flow_coefficient import valve_flow
 import numpy as np
@@ -18,22 +18,16 @@ class ExcelFr(csv.excel):
     delimiter = ';'
 
 
-class DataItem:
-    def __init__(self, var: Any, is_ok: bool = False) -> None:
-        self.val = var
-        self.is_ok = is_ok
-
-    def set(self, value: Any, is_ok: bool = True):
-        self.val = value
-        self.is_ok = is_ok
-
-
 class Share:
     lock = threading.Lock()
-    p_amont_vl = DataItem(0.0)
-    p_aval_vl = DataItem(0.0)
-    pos_vl = DataItem(0.0)
-    q_vl = DataItem(0.0)
+    p_amont_vl = None
+    p_aval_vl = None
+    pos_vl = None
+    q_vl = None
+    p_amont_vl_stack = deque(maxlen=360)
+    p_aval_vl_stack = deque(maxlen=360)
+    pos_vl_stack = deque(maxlen=360)
+    q_vl_stack = deque(maxlen=360)
 
     @classmethod
     def mbus_poll_thread(cls):
@@ -44,6 +38,8 @@ class Share:
         # coefs src: VL Oppy
         cv_poly = np.poly1d([-1.75115535e-10,  6.23498970e-08, -8.64511406e-06,  5.65364659e-04,
                              -1.61157646e-02,  1.79337632e-01,  7.87619448e-01, -3.60037881e-03])
+        # init stacks last feed time
+        last_q_feed_t = time.monotonic()
         # polling loop
         while True:
             # do modbus reading on socket
@@ -51,18 +47,25 @@ class Share:
             # if read is ok, store result in regs (with thread lock)
             with cls.lock:
                 if read_flt_l:
-                    cls.p_amont_vl.set(read_flt_l[0])
-                    cls.p_aval_vl.set(read_flt_l[1])
-                    cls.pos_vl.set(read_flt_l[2])
+                    cls.p_amont_vl = read_flt_l[0]
+                    cls.p_aval_vl = read_flt_l[1]
+                    cls.pos_vl = read_flt_l[2]
                     try:
-                        cls.q_vl.set(valve_flow(abs(cv_poly(cls.pos_vl.val)), cls.p_amont_vl.val, cls.p_aval_vl.val))
-                    except ValueError:
-                        cls.q_vl.is_ok = False
+                        cls.q_vl = (valve_flow(abs(cv_poly(cls.pos_vl)), cls.p_amont_vl, cls.p_aval_vl))
+                    except (ValueError, TypeError):
+                        cls.q_vl = None
                 else:
-                    cls.p_amont_vl.is_ok = False
-                    cls.p_aval_vl.is_ok = False
-                    cls.pos_vl.is_ok = False
-                    cls.q_vl.is_ok = False
+                    cls.p_amont_vl = None
+                    cls.p_aval_vl = None
+                    cls.pos_vl = None
+                    cls.q_vl = None
+                # feed the stacks every 10s
+                if time.monotonic() - last_q_feed_t > 10.0:
+                    last_q_feed_t = time.monotonic()
+                    Share.p_amont_vl_stack.append(Share.p_amont_vl)
+                    Share.p_aval_vl_stack.append(Share.p_aval_vl)
+                    Share.pos_vl_stack.append(Share.pos_vl)
+                    Share.q_vl_stack.append(Share.q_vl)
             # 1s before next polling
             time.sleep(1.0)
 
@@ -92,34 +95,48 @@ class HmiApp(tk.Tk):
 
     def txt_update_loop(self):
         # define widget update list
-        widgets_l = [(self.amt_txt, '%.2f bars', Share.p_amont_vl),
-                     (self.avl_txt, '%.2f bars', Share.p_aval_vl),
-                     (self.pos_txt, '%.2f %%', Share.pos_vl),
-                     (self.qvl_txt, 'Estimation débit VL %.0f nm3/h', Share.q_vl),]
+        widgets_l = [(self.amt_txt, '%.2f bars', Share.p_amont_vl, 0.0),
+                     (self.avl_txt, '%.2f bars', Share.p_aval_vl, 0.0),
+                     (self.pos_txt, '%.2f %%', Share.pos_vl, 0.0),
+                     (self.qvl_txt, 'Estimation débit VL %.0f nm3/h', Share.q_vl, 0),]
         # update text widgets
         with Share.lock:
-            for txt_wdg, txt_fmt, d_item in widgets_l:
-                self.cvs.itemconfig(txt_wdg, text=txt_fmt % d_item.val)
-                self.cvs.itemconfig(txt_wdg, fill='black' if d_item.is_ok else 'red')
+            for wdg_txt, fmt, current_val, default_val in widgets_l:
+                # fill text widget with current values or set text as red
+                if current_val is None:
+                    self.cvs.itemconfig(wdg_txt, fill='red')
+                else:
+                    self.cvs.itemconfig(wdg_txt, text=fmt % current_val)
+                    self.cvs.itemconfig(wdg_txt, fill='black')
+                # avoid empty text area
+                if not self.cvs.itemcget(wdg_txt, 'text'):
+                    self.cvs.itemconfig(wdg_txt, text=fmt % default_val)
         # refresh txt widgets in 500 ms
         self.after(ms=500, func=self.txt_update_loop)
 
     def csv_export_loop(self):
+        # define the export list [(fieldname, value, nb of digits), ]
+        export_l = [('p_amont', Share.p_amont_vl, 2), ('p_aval', Share.p_aval_vl, 2),
+                    ('position_vl', Share.pos_vl, 2), ('q_vl', Share.q_vl, 0)]
+        # build CSV export dict
+        csv_d = dict()
+        csv_d['datetime'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        with Share.lock:
+            for fieldname, value, digits in export_l:
+                try:
+                    csv_d[fieldname] = round(value, digits) if digits else round(value)
+                except TypeError:
+                    csv_d[fieldname] = float('nan')
         try:
             with open('export.csv', 'a', newline='') as csv_file:
                 # init CSV writer
-                fields_l = ['datetime', 'p_amont', 'p_aval', 'position_vl', 'q_vl']
                 csv_w = csv.DictWriter(csv_file, extrasaction='ignore', dialect='excel-fr',
-                                       fieldnames=fields_l)
+                                       fieldnames=csv_d.keys())
                 # add headers for a new file
                 if csv_file.tell() == 0:
                     csv_w.writeheader()
-                # add current data items to the CSV
-                csv_w.writerow({'datetime': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                'p_amont': round(Share.p_amont_vl.val, 2),
-                                'p_aval': round(Share.p_aval_vl.val, 2),
-                                'position_vl': round(Share.pos_vl.val, 2),
-                                'q_vl': round(Share.q_vl.val)})
+                # add current row
+                csv_w.writerow(csv_d)
         except PermissionError:
             pass
         # refresh csv every 10s
