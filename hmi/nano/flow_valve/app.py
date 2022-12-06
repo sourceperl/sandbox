57964f89761403ec58f0fb5e60c42437139c9147
sourceperl <3809271+sourceperl@users.noreply.github.com>
2022-12-06 19:39:54 +0100

    update flow_valve nano hmi

--- hmi/nano/flow_valve/app.py
+++ hmi/nano/flow_valve/app.py
@@ -11,6 +11,11 @@ from lib.modbus import FloatModbusClient
 from lib.flow_coefficient import valve_flow
 import numpy as np
 import tkinter as tk
+from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.animation import FuncAnimation
+from matplotlib import style
 
 
 # some class
@@ -23,11 +28,13 @@ class Share:
     p_amont_vl = None
     p_aval_vl = None
     pos_vl = None
+    sp_p_aval = None
     q_vl = None
     p_amont_vl_stack = deque(maxlen=360)
     p_aval_vl_stack = deque(maxlen=360)
     pos_vl_stack = deque(maxlen=360)
     q_vl_stack = deque(maxlen=360)
+    sp_p_aval_stack = deque(maxlen=360)
 
     @classmethod
     def mbus_poll_thread(cls):
@@ -43,21 +50,19 @@ class Share:
         # polling loop
         while True:
             # do modbus reading on socket
-            read_flt_l = fmc.read_float(0, 3)
+            rfloat_d = dict()
+            for addr in (804, 808, 812, 816):
+                result = fmc.read_float(addr)
+                rfloat_d[addr] = result[0] if result is not None else None
             # if read is ok, store result in regs (with thread lock)
             with cls.lock:
-                if read_flt_l:
-                    cls.p_amont_vl = read_flt_l[0]
-                    cls.p_aval_vl = read_flt_l[1]
-                    cls.pos_vl = read_flt_l[2]
-                    try:
-                        cls.q_vl = (valve_flow(abs(cv_poly(cls.pos_vl)), cls.p_amont_vl, cls.p_aval_vl))
-                    except (ValueError, TypeError):
-                        cls.q_vl = None
-                else:
-                    cls.p_amont_vl = None
-                    cls.p_aval_vl = None
-                    cls.pos_vl = None
+                cls.p_amont_vl = rfloat_d.get(804)
+                cls.p_aval_vl = rfloat_d.get(808)
+                cls.pos_vl = rfloat_d.get(812)
+                cls.sp_p_aval = rfloat_d.get(816)
+                try:
+                    cls.q_vl = (valve_flow(max(cv_poly(cls.pos_vl), 0.0), cls.p_amont_vl, cls.p_aval_vl))
+                except (ValueError, TypeError):
                     cls.q_vl = None
                 # feed the stacks every 10s
                 if time.monotonic() - last_q_feed_t > 10.0:
@@ -70,13 +75,74 @@ class Share:
             time.sleep(1.0)
 
 
-class HmiApp(tk.Tk):
-    """Tk HMI app"""
+class TrendPamontFrame(tk.Frame):
+    # some const
+    WIDTH = 400
+    HEIGHT = 300
+    DPI = 120
 
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        # configure main window
-        self.wm_title('Nano HMI flow valve')
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+        # init matplotlib graph
+        style.use('ggplot')
+        self.fig = Figure(figsize=(self.WIDTH/self.DPI, self.HEIGHT/self.DPI), dpi=self.DPI)
+        self.plt = self.fig.add_subplot()
+        self.fig.set_tight_layout(True)
+        # add animate graph widget to tk app
+        graph = FigureCanvasTkAgg(self.fig, master=self)
+        self.can_graph = graph.get_tk_widget()
+        self.can_graph.pack()
+        # init plot refresh function
+        self.func_anim = FuncAnimation(self.fig, self.update_graph, interval=1000)
+
+    def update_graph(self, _):
+        # decrease CPU usage if not mapped
+        if self.winfo_ismapped():
+            self.plt.clear()
+            self.plt.set_ylabel('bars')
+            with Share.lock:
+                self.plt.plot(Share.p_amont_vl_stack, 'r-')
+
+
+class TrendPavalFrame(tk.Frame):
+    # some const
+    WIDTH = 400
+    HEIGHT = 300
+    DPI = 120
+
+    def __init__(self, parent, *args, **kwargs):
+        # heritage stuff
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+        # init matplotlib graph
+        style.use('ggplot')
+        self.fig = Figure(figsize=(self.WIDTH/self.DPI, self.HEIGHT/self.DPI), dpi=self.DPI)
+        self.plt = self.fig.add_subplot()
+        self.fig.set_tight_layout(True)
+        # add animate graph widget to tk app
+        graph = FigureCanvasTkAgg(self.fig, master=self)
+        self.can_graph = graph.get_tk_widget()
+        self.can_graph.pack()
+        # init plot refresh function
+        self.func_anim = FuncAnimation(self.fig, self.update_graph, interval=1000)
+
+    def update_graph(self, _):
+        # decrease CPU usage if not mapped
+        if self.winfo_ismapped():
+            self.plt.clear()
+            self.plt.set_ylabel('bars')
+            with Share.lock:
+                self.plt.plot(Share.p_amont_vl_stack, 'b-')
+
+
+class TabSyno(tk.Frame):
+    """Synoptic tab class"""
+
+    def __init__(self, parent, *args, **kwargs):
+        # heritage stuff
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
         # add canvas
         self.cvs = tk.Canvas(self, width=960, height=720)
         self.cvs.pack()
@@ -84,35 +150,104 @@ class HmiApp(tk.Tk):
         self._photo_img = tk.PhotoImage(file='img/background.png')
         self.cvs.create_image(0, 0, anchor=tk.NW, image=self._photo_img)
         # add an overlay with text fields
-        self.amt_txt = self.cvs.create_text(733, 340, font=('Helvetica', 36))
-        self.avl_txt = self.cvs.create_text(227, 340, font=('Helvetica', 36))
-        self.pos_txt = self.cvs.create_text(480, 530, font=('Helvetica', 36))
-        self.qvl_txt = self.cvs.create_text(480, 660, font=('Helvetica', 36))
+        self.amt_txt = self.cvs.create_text(733, 430, font=('Helvetica', 36))
+        self.avl_txt = self.cvs.create_text(227, 430, font=('Helvetica', 36))
+        self.pos_txt = self.cvs.create_text(480, 610, font=('Helvetica', 36))
+        self.qvl_txt = self.cvs.create_text(480, 680, font=('Helvetica', 36))
+        # embed matplotlib frame in canvas
+        self.fr_gph_amt = TrendPamontFrame(self)
+        self.cvs.create_window(750, 175, window=self.fr_gph_amt)
+        self.fr_gph_avl = TrendPavalFrame(self)
+        self.cvs.create_window(200, 175, window=self.fr_gph_avl)
         # start widget update loop
         self.after(ms=500, func=self.txt_update_loop)
-        # start csv export loop
-        self.after(ms=2000, func=self.csv_export_loop)
 
     def txt_update_loop(self):
-        # define widget update list
-        widgets_l = [(self.amt_txt, '%.2f bars', Share.p_amont_vl, 0.0),
-                     (self.avl_txt, '%.2f bars', Share.p_aval_vl, 0.0),
-                     (self.pos_txt, '%.2f %%', Share.pos_vl, 0.0),
-                     (self.qvl_txt, 'Estimation débit VL %.0f nm3/h', Share.q_vl, 0),]
-        # update text widgets
-        with Share.lock:
-            for wdg_txt, fmt, current_val, default_val in widgets_l:
-                # fill text widget with current values or set text as red
-                if current_val is None:
-                    self.cvs.itemconfig(wdg_txt, fill='red')
-                else:
-                    self.cvs.itemconfig(wdg_txt, text=fmt % current_val)
-                    self.cvs.itemconfig(wdg_txt, fill='black')
-                # avoid empty text area
-                if not self.cvs.itemcget(wdg_txt, 'text'):
-                    self.cvs.itemconfig(wdg_txt, text=fmt % default_val)
+        # decrease CPU usage if not mapped
+        if self.winfo_ismapped():
+            # define widget update list
+            widgets_l = [(self.amt_txt, '%.2f bars', Share.p_amont_vl, 0.0),
+                         (self.avl_txt, '%.2f bars', Share.p_aval_vl, 0.0),
+                         (self.pos_txt, '%.2f %%', Share.pos_vl, 0.0),
+                         (self.qvl_txt, 'Estimation débit VL %.0f nm3/h', Share.q_vl, 0),]
+            # update text widgets
+            with Share.lock:
+                for wdg_txt, fmt, current_val, default_val in widgets_l:
+                    # fill text widget with current values or set text as red
+                    if current_val is None:
+                        self.cvs.itemconfig(wdg_txt, fill='red')
+                    else:
+                        self.cvs.itemconfig(wdg_txt, text=fmt % current_val)
+                        self.cvs.itemconfig(wdg_txt, fill='black')
+                    # avoid empty text area
+                    if not self.cvs.itemcget(wdg_txt, 'text'):
+                        self.cvs.itemconfig(wdg_txt, text=fmt % default_val)
         # refresh txt widgets in 500 ms
         self.after(ms=500, func=self.txt_update_loop)
+
+
+class TabGraph(tk.Frame):
+    # some const
+    WIDTH = 960
+    HEIGHT = 720
+    DPI = 120
+
+    def __init__(self, parent, *args, **kwargs):
+        # heritage stuff
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+        # init matplotlib graph
+        style.use('ggplot')
+        self.fig = Figure(figsize=(self.WIDTH/self.DPI, self.HEIGHT/self.DPI), dpi=self.DPI)
+        self.plt_h = self.fig.add_subplot(211)
+        self.plt_b = self.fig.add_subplot(212)
+        self.fig.set_tight_layout(True)
+        # add animate graph widget to tk app
+        graph = FigureCanvasTkAgg(self.fig, master=self)
+        self.can_graph = graph.get_tk_widget()
+        self.can_graph.pack()
+        # init plot refresh function
+        self.func_anim = FuncAnimation(self.fig, self.update_graph, interval=1000)
+
+    def update_graph(self, _):
+        # decrease CPU usage if not mapped
+        if self.winfo_ismapped():
+            self.plt_h.clear()
+            self.plt_h.set_title('Historique pressions')
+            self.plt_h.set_ylabel('bars')
+            self.plt_b.clear()
+            self.plt_b.set_title('Historique position VL')
+            self.plt_b.set_ylabel('%')
+            with Share.lock:
+                self.plt_h.plot(Share.p_amont_vl_stack, 'r-')
+                self.plt_h.plot(Share.p_aval_vl_stack, 'b-')
+                self.plt_b.plot(Share.pos_vl_stack, 'g-')
+
+
+class HmiApp(tk.Tk):
+    """Tk HMI app"""
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        # configure main window
+        self.wm_title('Nano HMI flow valve')
+        # build a notebook with tabs
+        self.note = ttk.Notebook(self)
+        self.tab_syno = TabSyno(self.note)
+        self.tab_graph = TabGraph(self.note)
+        self.note.add(self.tab_syno, text='Synoptique (F1)')
+        self.note.add(self.tab_graph, text='Graphiques (F2)')
+        self.note.pack(fill=tk.BOTH, expand=True)
+        # defaut selected tab
+        self.note.select(self.tab_syno)
+        # bind function keys to tabs
+        self.bind('<F1>', lambda evt: self.note.select(self.tab_syno))
+        self.bind('<F2>', lambda evt: self.note.select(self.tab_graph))
+        # add an exit button
+        self.but_exit = tk.Button(self, text="Exit", command=lambda: self.destroy())
+        self.but_exit.pack(side=tk.RIGHT, padx=10, pady=5)
+        # start csv export loop
+        self.after(ms=2000, func=self.csv_export_loop)
 
     def csv_export_loop(self):
         # define the export list [(fieldname, value, nb of digits), ]
@@ -127,6 +262,7 @@ class HmiApp(tk.Tk):
                     csv_d[fieldname] = round(value, digits) if digits else round(value)
                 except TypeError:
                     csv_d[fieldname] = float('nan')
+        # add CSV export dict data to 'export.csv' file
         try:
             with open('export.csv', 'a', newline='') as csv_file:
                 # init CSV writer
