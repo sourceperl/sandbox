3678a118b50d44f1f38764558a40e7769282f21a
sourceperl <3809271+sourceperl@users.noreply.github.com>
2023-03-01 19:34:35 +0100

    update to_syslog.py

--- log/to_syslog.py
+++ log/to_syslog.py
@@ -3,15 +3,25 @@
 """Connect logging module to syslog."""
 
 import logging
-from logging import StreamHandler
+from logging import Formatter, StreamHandler
 from logging.handlers import SysLogHandler
+import sys
 
 
 # some const
 APP_NAME = 'to_syslog_test'
 
 
-# add default handler (stderr) and syslog handler (append message to /var/log/syslog)
-handlers = [StreamHandler(), SysLogHandler(address='/dev/log')]
-logging.basicConfig(format=f'{APP_NAME}[%(process)d]: %(levelname)-8s %(message)s', level=logging.DEBUG, handlers=handlers)
+# add default handler (stderr)
+def_hdl = StreamHandler()
+def_hdl.setLevel(logging.DEBUG)
+def_hdl.setFormatter(Formatter('%(asctime)s %(levelname)-8s %(message)s'))
+handlers = [def_hdl]
+# add syslog handler (append message to /var/log/syslog) on linux system
+if sys.platform.startswith('linux'):
+    syslog_hdl = SysLogHandler(address='/dev/log')
+    syslog_hdl.setLevel(logging.INFO)
+    syslog_hdl.setFormatter(Formatter(f'{APP_NAME}[%(process)d]: %(message)s'))
+    handlers.append(syslog_hdl)
+logging.basicConfig(level=logging.DEBUG, handlers=handlers)
 logging.info('a test message')
