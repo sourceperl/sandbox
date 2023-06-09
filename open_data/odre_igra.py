251c5d6b6a4705772512c168f74319cbb927392a
sourceperl <3809271+sourceperl@users.noreply.github.com>
2023-06-09 14:05:33 +0200

    update odre_igra.py

--- open_data/odre_igra.py
+++ open_data/odre_igra.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 
 # some const
 SEARCH_URL = 'https://odre.opendatasoft.com/api/records/1.0/search/?'
+DPI = 96
+FIG_SIZE = (546/DPI, 227/DPI)
 
 
 try:
@@ -26,19 +28,20 @@ try:
         item_year, item_month = map(str, record['fields']['date'].split('-')[:2])
         item_igra = float(record['fields']['igra'])
         items_l.append((item_year, item_month, item_igra))
-    # convert items list and plot it
-    x, y = [], []
+    # convert items list for plot
+    x, heights = [], []
     for year, month, igra in sorted(items_l):
         x.append(f'{month}')
-        y.append(igra)
-    plt.bar(x, y)
+        heights.append(igra)
+    # set figure size and draw bar plot
+    plt.figure(figsize=FIG_SIZE, dpi=DPI)
+    plt.bar(x, heights)
     # add value as labels in bars
-    for i, value in enumerate(y):
-        plt.text(i, value/2, f'{value:.01f}', ha='center', bbox=dict(facecolor='white', alpha=0.5))
+    for i, h in enumerate(heights):
+        plt.text(i, h/2, f'{h:.01f}', ha='center', bbox=dict(facecolor='white', alpha=0.5))
     # title
     plt.title('Indicateur annualisé gaz renouvelable dans les Hauts-de-France')
     # axes labels
-    plt.xlabel('Mois de l\'année')
     plt.ylabel('IGRa (%)')
     plt.show()
 except (KeyError, TypeError, URLError) as e:
