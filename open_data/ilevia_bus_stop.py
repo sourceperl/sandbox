59077ffbd15be6482336809fc881f68efed5f454
sourceperl <3809271+sourceperl@users.noreply.github.com>
2023-08-25 13:53:40 +0200

    update ilevia_bus_stop.py

--- open_data/ilevia_bus_stop.py
+++ open_data/ilevia_bus_stop.py
@@ -2,7 +2,7 @@
 
 """Ask for the next departure from an Ilevia bus stop."""
 
-from datetime import datetime, timezone
+from datetime import datetime
 import json
 from urllib.error import URLError
 from urllib.parse import urlencode
@@ -15,7 +15,7 @@ BASE_URL = 'https://opendata.lillemetropole.fr/api/explore/v2.1/catalog/datasets
 
 # request data from MEL opendata platform
 try:
-    search_args = {'order_by': 'heureestimeedepart', 'refine': 'nomstation:GARE LILLE FLANDRES'}
+    search_args = {'order_by': 'heureestimeedepart', 'refine': 'nomstation:REPUBLIQUE BEAUX ARTS', 'limit': '100'}
     url = BASE_URL + urlencode(search_args)
     response = urlopen(url, timeout=4.0)
     # check HTTP response code
@@ -23,11 +23,19 @@ try:
         raise ValueError('unable to get data from HTTP server')
     # convert response
     js_d = json.loads(response.read())
+    deps_l = []
     for record in js_d['results']:
         line_id = record['codeligne']
         dest_id = record['sensligne']
-        update_dt = datetime.fromisoformat(record['datemodification']).replace(tzinfo=timezone.utc).astimezone(tz=None)
-        start_dt = datetime.fromisoformat(record['heureestimeedepart']).replace(tzinfo=timezone.utc).astimezone(tz=None)
-        print(f'{line_id:6} {dest_id:28} start at {start_dt!s}   [update {update_dt!s}]')
+        station_id = record['identifiantstation']
+        start_dt = datetime.fromisoformat(record['heureestimeedepart']).astimezone(tz=None)
+        update_dt = datetime.fromisoformat(record['datemodification']).astimezone(tz=None)
+        deps_l.append(dict(line_id=line_id, dest_id=dest_id, station_id=station_id,
+                           start_dt=start_dt, update_dt=update_dt))
+    # show results
+    for dep_d in deps_l:
+        msg = (f"{dep_d['start_dt'].strftime('%H:%M:%S'):10}"
+               f"{dep_d['line_id']:6} {dep_d['dest_id']:32}")
+        print(msg)
 except (KeyError, ValueError, URLError) as e:
     print(f'error occur: {e!r}')
