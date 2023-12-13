06438028f50f74f102c927f5e73fa0f5a0620a23
sourceperl <3809271+sourceperl@users.noreply.github.com>
2023-12-13 15:24:28 +0100

    update victoriametrics/

--- misc/test_regex.py
+++ misc/test_regex.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
 
-""" Test of regex.
+""" Unit test of regex."""
 
-- just pass string(s) as args on command line to test it.
-"""
-
-from pprint import pprint
 import re
-import sys
+import unittest
 
 
-for test_str in sys.argv[1:]:
-    print(f'test "{test_str}"')
-    result = re.search(r'^(?!__.*)(?P<name>[a-zA-Z_:][a-zA-Z0-9_:]*)(?:{(?P<labels>[^}]*)})?$', test_str)
+class TestMetricRegex(unittest.TestCase):
+    pattern = re.compile(r'(?!__.*)(?P<name>[a-zA-Z_:][a-zA-Z0-9_:]*)(?:{(?P<labels>.*?)})?')
+    match_l = ['my_metric', 'my_metric{tag="foo"}', ]
+    no_match_l = ['__my_metric', 'my_m$etric{}', ]
 
-    if result:
-        print(f'\tfind name: {result.group("name")}')
-        print(f'\tfind labels: {result.group("labels")}')
-    else:
-        print('no match')
-    print('')
+    def test_match(self):
+        for test_str in self.match_l:
+            self.assertTrue(self.pattern.fullmatch(test_str))
+
+    def test_no_match(self):
+        for test_str in self.no_match_l:
+            self.assertFalse(self.pattern.fullmatch(test_str))
+
+
+class TestLabelsRegex(unittest.TestCase):
+    pattern = re.compile(r'(?:(?!__.*)[a-zA-Z_][a-zA-Z0-9_]*="[^"]*",?)+')
+    match_l = ['tag="foo"', 'tag="foo",', 'truc="machin",bidule="42"', 'up="test",foo="value"']
+    no_match_l = ['__tag="foo"', 'tag=foo,', 'tag="foo"""', 'tag="z",,']
+
+    def test_match(self):
+        for test_str in self.match_l:
+            self.assertTrue(self.pattern.fullmatch(test_str))
+
+    def test_no_match(self):
+        for test_str in self.no_match_l:
+            self.assertFalse(self.pattern.fullmatch(test_str))
+
+
+if __name__ == '__main__':
+    unittest.main()
