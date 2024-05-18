0d25c64ee7990e64fff461c3f736a5dc603af3fc
sourceperl <3809271+sourceperl@users.noreply.github.com>
2024-05-18 19:50:29 +0200

    add campi-flegrei-events.py to open_data/

---
+++ open_data/campi-flegrei-events.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+""" Request and process data about campi flegrei events from INGV. """
+
+import csv
+from datetime import timezone
+import urllib.error
+from urllib.request import urlopen
+from dateutil.parser import parse, ParserError
+
+
+# some const
+DATA_URL = 'https://terremoti.ov.ingv.it/gossip/flegrei/2024/events.csv'
+
+
+try:
+    # request CSV data from web server
+    resp = urlopen(DATA_URL, timeout=4.0)
+    lines_l = resp.read().decode().splitlines()
+    # extract
+    for row in csv.DictReader(lines_l, delimiter=','):
+        try:
+            event_id = int(row['#EventID'])
+            event_dt = parse(row['Time']).replace(tzinfo=timezone.utc)
+            try:
+                depth_km = float(row['Depth/km'])
+            except ValueError:
+                depth_km = None
+            try:
+                mag = float(row['Magnitude'])
+            except ValueError:
+                mag = None
+            if mag and mag > 2.0:
+                event_dt_str = event_dt.astimezone().isoformat(timespec='seconds')
+                print(f'{event_dt_str} magnitude: {mag} (depth: {depth_km:.1f} km)')
+        except (ParserError, KeyError):
+            pass
+except (urllib.error.URLError, ValueError) as e:
+    print(f'error occur: {e!r}')
