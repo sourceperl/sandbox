4ec6eff7c1e10ea3f75d3ebdce4ecde965b83ef6
sourceperl <3809271+sourceperl@users.noreply.github.com>
2025-05-26 09:45:30 +0200

    update victoriametrics/

--- victoriametrics/setup/latest.sh
+++ victoriametrics/setup/latest.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-# Utiliser curl pour récupérer les informations de la dernière version
-LATEST=$(curl -s https://api.github.com/repos/VictoriaMetrics/VictoriaMetrics/releases/latest | jq -r .tag_name)
 
-# Extraire le tag_name qui contient la version
+LATEST=$(curl -s https://api.github.com/repos/VictoriaMetrics/VictoriaMetrics/releases/latest | jq -r .tag_name)
 echo "the latest available version of VictoriaMetrics is ${LATEST}"
