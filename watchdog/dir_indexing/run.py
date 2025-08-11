db614971fe048d7d55a21f94b6b22e179d57e150
sourceperl <3809271+sourceperl@users.noreply.github.com>
2025-08-11 11:40:01 +0200

    update watchdog/

--- watchdog/dir_indexing/run.py
+++ watchdog/dir_indexing/run.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
 import argparse
 import sys
 
 from app import main
-from private_data import WATCHED_DIR
 
 # parse command line args
 parser = argparse.ArgumentParser()
+parser.add_argument('watched_directory', type=str, help='path to the watched directory')
 parser.add_argument('-d', '--debug', action='store_true', help='set debug mode')
 args = parser.parse_args()
 
 # main (index files as "file.txt" but not as "_file.txt")
-sys.exit(main(watched_dir=WATCHED_DIR, allow_patterns=['*.txt'], skip_patterns=['_*'], debug=args.debug))
+sys.exit(main(watched_dir=args.watched_directory, skip_patterns=['_*'], debug=args.debug))
