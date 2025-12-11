14861170112e508d375967ec100e7b1de183a3e1
sourceperl <3809271+sourceperl@users.noreply.github.com>
2025-12-11 16:10:20 +0100

    update signal/

--- signal/shape/euclid_shape_finder_3.py
+++ signal/shape/euclid_shape_finder_3.py
@@ -15,61 +15,62 @@ from datasets.sig_gallery import sig_exp_pulse
 
 # some class
 class ShapeFinderAnim:
-    def __init__(self, frames: int, window_size: int, threshold: float = 1.0) -> None:
-        self.frames = frames
-        self.window_size = window_size
+    def __init__(self, raw_sig: np.ndarray, target_pat: np.ndarray, threshold: float) -> None:
+        # public
+        self.raw_sig = raw_sig
+        self.target_pat = target_pat
         self.threshold = threshold
-        # load RAW signal from txt file
-        self.raw_sig = 50 - 5*sig_exp_pulse(pulse_len=50, prefix_len=60, suffix_len=100)
-        # target signal
-        # self.sig_target = np.zeros(self.window_size)
-        # self.sig_target[20:25] = -100
-        # self.sig_target[25:30] = -50
-        # self.sig_target[30:40] = -20
-        # self.sig_target[40:50] = -10
-        self.sig_target = -100*sig_exp_pulse(pulse_len=30, prefix_len=20, suffix_len=100)[:self.window_size]
+        # private
+        self._window_size = len(self.target_pat)
+        self._frames = len(self.raw_sig) - self._window_size
+
         # x axis
-        self.x_axes = np.arange(self.window_size)
+        self.x_axes = np.arange(self._window_size)
+
         # init subplots
         self.fig, (self.ax1, self.ax2, self.ax3) = plt.subplots(nrows=3, ncols=1)
-        self.fig.suptitle('Signal shape finder')
-        self.ax3_y = [None] * self.frames
+        self.fig.suptitle('Signal shape finder (euclidean distance)')
+        self.ax3_y = []
 
     # plot animation function
     def _anim(self, frame: int):
         # to numpy
         start_at = frame
-        raw_sig_part = self.raw_sig[start_at:self.window_size + start_at]
+        raw_sig_part = self.raw_sig[start_at:self._window_size + start_at]
 
         # normalize signal
         sig = 100 * (raw_sig_part - raw_sig_part.min()) / (raw_sig_part.max()-raw_sig_part.min()) - 100
 
         # check current limit and signal status
-        square_overshoot = (sig - self.sig_target)**2
+        square_overshoot = (sig - self.target_pat)**2
         mean_overshoot = np.sqrt(square_overshoot.mean())
         sig_part_match = mean_overshoot < self.threshold
 
         # ax infos
         ax_color = 'green' if sig_part_match else 'red'
-        ax_label = f'mean overshoot={mean_overshoot:.2f} % (frame {frame+1}/{self.frames})'
+        ax_label = f'mean overshoot={mean_overshoot:.2f} % (frame {frame+1}/{self._frames})'
 
         # ax1 build
         self.ax1.clear()
-        self.ax1.plot(raw_sig_part)
+        self.ax1.plot(self.raw_sig, color='gray', label='Full RAW signal')
+        self.ax1.plot(np.arange(self._window_size) + start_at, raw_sig_part,
+                      color='blue', linewidth=2, label='Section analyzed')
+        self.ax1.fill_between(np.arange(self._window_size) + start_at, raw_sig_part.min(),
+                              raw_sig_part.max(), color=ax_color, alpha=0.1)
         self.ax1.set_ylabel('RAW signal value')
         self.ax1.grid()
 
         # ax2 build
         self.ax2.clear()
-        self.ax2.plot(sig, label='signal in %')
-        self.ax2.plot(self.sig_target, 'r--', label='max')
-        self.ax2.fill_between(self.x_axes, sig, self.sig_target, color=ax_color, alpha=0.3)
+        self.ax2.plot(sig, label='Signal')
+        self.ax2.plot(self.target_pat, 'r--', label='Target pattern')
+        self.ax2.fill_between(self.x_axes, sig, self.target_pat, color=ax_color, alpha=0.3)
         self.ax2.set_ylabel('signal (in %)')
         self.ax2.legend()
         self.ax2.grid()
 
         # ax3 build
-        self.ax3_y[frame] = mean_overshoot
+        self.ax3_y.append(mean_overshoot)
         self.ax3.plot(self.ax3_y, color='orange')
         self.ax3.axhline(self.threshold, linestyle='--', color='green')
         self.ax3.set_xlabel(ax_label, fontdict={'color': ax_color})
@@ -78,15 +79,19 @@ class ShapeFinderAnim:
 
         return self.ax1, self.ax2, self.ax3
 
-    def run(self):
-        ani = animation.FuncAnimation(self.fig, self._anim, frames=self.frames, interval=150, repeat=False)
-        # movie save (duration (in s) = frames * 1 / fps)
-        # mp4_path = self.data_file_path.parent / 'out.mp4'
-        # ani.save(mp4_path, fps=10, dpi=300)
-        # display animation (duration (in s) = frames * interval / 1000)
+    def __call__(self):
+        ani = animation.FuncAnimation(self.fig, self._anim, frames=self._frames, interval=150, repeat=False)
         plt.show()
 
 
 if __name__ == '__main__':
-    # start animation
-    ShapeFinderAnim(frames=80, window_size=60).run()
+    # fake raw signal
+    raw_sig = 50 - 5*sig_exp_pulse(pulse_len=50, prefix_len=60, suffix_len=60)
+
+    # target pattern
+    target_pat = -100*sig_exp_pulse(pulse_len=50, prefix_len=10, suffix_len=0)
+
+    # detection threshold
+    threshold = 5.0
+
+    ShapeFinderAnim(raw_sig, target_pat, threshold)()
